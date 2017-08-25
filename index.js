6915476baf603d19d5a56119be8fc5dcad82301b
PauloJorge <paulojorge@LAPTOP-BO8D2IR5.home>
2017-08-25 04:56:44 -0300

    25/08

--- index.js
+++ index.js
@@ -31,6 +31,15 @@ class ImageMessage extends Message {
   }
 }
 
+var text = 'Some text';
+var created = Date.now();
+
+var duckTypeMessage = {
+  text,
+  created
+};
+console.log(duckTypeMessage);
+
 var emptyMessage = new Message();
 var textMessage = new Message('Yesterday Message', Date.now() - 86400);
 var photoMessage = new ImageMessage();
@@ -39,3 +48,10 @@ console.log(String(emptyMessage));
 console.log(String(textMessage));
 console.log(String(photoMessage));
 
+console.log(emptyMessage instanceof Message);
+console.log(textMessage instanceof Message);
+console.log(photoMessage instanceof Message);
+console.log(photoMessage instanceof ImageMessage);
+
+console.log(textMessage instanceof ImageMessage);
+console.log(duckTypeMessage instanceof ImageMessage);
